58f2f7e95b2d628890a24906aeb31dd1f43ba287
Ragnar Englund <ragnar.englund@annotell.com>
2021-08-05 11:35:27 +0200

    Added method to get inputs using only uuids

--- annotell-input-api/tests/get_inputs_test.py
+++ annotell-input-api/tests/get_inputs_test.py
@@ -1,24 +1,37 @@
 from typing import List
+import time
 import annotell.input_api.model as IAM
 import examples.get_inputs as get_inputs_example
+import examples.get_inputs_by_uuids as get_inputs_by_uuids_example
 import annotell.input_api.input_api_client as IAC
+import examples.cameras as cameras_example
 from tests.utils import TestProjects
 
 
 class TestInput:
 
     @staticmethod
-    def filter_projects(projects: List[IAM.Project]):
+    def filter_cameras_project(projects: List[IAM.Project]):
         return [p for p in projects if p.project == TestProjects.CamerasProject]
 
-    def test_get_inputs(self, client: IAC.InputApiClient):
+    def test_get_inputs_for_project(self, client: IAC.InputApiClient):
         projects = client.project.get_projects()
-        project = self.filter_projects(projects)[0].project
+        project = self.filter_cameras_project(projects)[0].project
         project_inputs = get_inputs_example.run(client=client, project=project)
-        assert isinstance(project_inputs, list)
 
+        assert isinstance(project_inputs, list)
         assert len(project_inputs) >= 1
 
-        input = get_inputs_example.run(client=client, project=project, input_uuids=[project_inputs[0].uuid])
+    def test_get_inputs_with_input_uuid(self, client: IAC.InputApiClient):
+        projects = client.project.get_projects()
+        project = self.filter_cameras_project(projects)[0].project
+        input_response = cameras_example.run(client=client, project=project, dryrun=False)
+        input_uuid = input_response.input_uuid
 
-        assert len(input) == 1
+        assert isinstance(input_uuid, str)
+
+        print(input_uuid)
+        inputs = get_inputs_by_uuids_example.run(client=client, input_uuids=[input_uuid])
+
+        assert isinstance(inputs, list)
+        assert len(inputs) == 1
