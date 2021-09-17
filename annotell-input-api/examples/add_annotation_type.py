268c9b3f8695b8b77e45ce3435edf82ff31c8f2b
Ragnar Englund <ragnar.englund@annotell.com>
2021-09-17 12:06:24 +0200

    less space

--- annotell-input-api/examples/add_annotation_type.py
+++ annotell-input-api/examples/add_annotation_type.py
@@ -7,7 +7,6 @@ import annotell.input_api.input_api_client as IAC
 def run(client: IAC.InputApiClient, input_uuid: str, annotation_type: str):
     print("Adding annotation types to input...")
 
-
     client.input.add_annotation_type(input_uuid=input_uuid, annotation_type=annotation_type)
 
 
