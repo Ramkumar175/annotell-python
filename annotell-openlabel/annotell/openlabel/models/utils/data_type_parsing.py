19dabc85b05ab6ad4c223da9990df4e4fd077e49
Oskar Olin <oskar.olin@annotell.com>
2021-10-08 09:18:46 +0200

    Openlabel models for 1.0.0 (#94)
    * Removed old models and the tests for them
    
    * Automatically generated new models for OpenLABEL 1.0.0 from json schema using pydantic
    
    Co-authored-by: Alexander Lydh <alexander.lydh@annotell.com>

--- annotell-openlabel/annotell/openlabel/models/utils/data_type_parsing.py
+++ annotell-openlabel/annotell/openlabel/models/utils/data_type_parsing.py
@@ -1,9 +1,9 @@
 from typing import Any, Tuple
 
-from annotell.openlabel.models.data_types import Boolean, DataTypeBase, Num, Text, Vec
+import annotell.openlabel.models as OLM
 
 
-def get_openlabel_type(name: str, val: Any) -> Tuple[str, DataTypeBase]:
+def get_openlabel_type(name: str, val: Any) -> Tuple[str, OLM.DataTypeBase]:
 
     def isfloat(num: str) -> bool:
         try:
@@ -13,14 +13,14 @@ def get_openlabel_type(name: str, val: Any) -> Tuple[str, DataTypeBase]:
             return False
 
     if isinstance(val, bool):
-        return "boolean", Boolean(name=name, val=val)
+        return "boolean", OLM.Boolean(name=name, val=val)
     elif isinstance(val, list):
-        return "vec", Vec(name=name, val=val)
+        return "vec", OLM.Vec(name=name, val=val)
     elif isinstance(val, str):
-        return "text", Text(name=name, val=val)
+        return "text", OLM.Text(name=name, val=val)
     elif isinstance(val, int):
-        return "num", Num(name=name, val=val)
+        return "num", OLM.Num(name=name, val=val)
     elif isfloat(val):
-        return "num", Num(name=name, val=float(val))
+        return "num", OLM.Num(name=name, val=float(val))
 
     raise NotImplementedError
