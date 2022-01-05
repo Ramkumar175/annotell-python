aa2f0073ee39a45a761703595d5eea6652915b78
Ragnar Englund <ragnar.englund@annotell.com>
2022-01-05 08:55:13 +0100

    Fixed tests towards staging

--- annotell-input-api/tests/get_annotations_test.py
+++ annotell-input-api/tests/get_annotations_test.py
@@ -8,15 +8,16 @@ import examples.get_annotation as get_annotation_example
 import pytest
 from annotell.input_api.model.annotation.client_annotation import Annotation
 
+
 #TODO: Add client_annotations to testdata INT-663
 class TestGetAnnotations:
+
     def test_get_annotation(self, client: IAC.InputApiClient):
-        annotation = get_annotation_example.run(client=client, input_uuid="9c11cf44-e5a1-4367-a7a5-6ce0bf900763", annotation_type="signs")
+        annotation = get_annotation_example.run(client=client, input_uuid="e1229546-f447-4c07-8f6d-1347f067d14a", annotation_type="signs")
         assert isinstance(annotation, Annotation)
-        assert annotation.content["test"] == 1
 
     def test_get_annotation_incorrect_at(self, client: IAC.InputApiClient):
         with pytest.raises(Exception) as exception_info:
-            get_annotation_example.run(client=client, input_uuid="71d97ad7-d5c2-4751-a98a-22e17277a9b1", annotation_type="object")
+            get_annotation_example.run(client=client, input_uuid="e1229546-f447-4c07-8f6d-1347f067d14a", annotation_type="od")
 
         assert "404 Client Error: Not Found for url:" in exception_info.value.args[0]
--- annotell-input-api/tests/get_project_annotations_test.py
+++ annotell-input-api/tests/get_project_annotations_test.py
@@ -11,8 +11,10 @@ from tests.utils import TestProjects
 import examples.get_annotation as get_annotation_example
 import examples.get_project_annotations as get_project_annotations_example
 
+
 #TODO: Add client_annotations to testdata INT-663
 class TestGetAnnotations:
+
     @staticmethod
     def filter_cameras_project(projects: List[IAM.Project]):
         return [p for p in projects if p.project == TestProjects.CamerasProject]
@@ -23,7 +25,6 @@ class TestGetAnnotations:
         annotations = get_project_annotations_example.run(client=client, project=project, annotation_type="signs")
         annotation_list = list(annotations)
         assert len(annotation_list) > 0
-        assert annotation_list[0].content["test"] == 1
 
     def test_get_project_annotations_incorrect_at(self, client: IAC.InputApiClient):
         with pytest.raises(Exception) as exception_info:
@@ -31,4 +32,5 @@ class TestGetAnnotations:
             project = self.filter_cameras_project(projects)[0].project
             a = get_project_annotations_example.run(client=client, project=project, annotation_type="object")
             list(a)
-        assert exception_info.value.args[0] == "Validation failed: Invalid annotation type sent: object. Allowed values in project: [object-detection, signs]"
+        assert exception_info.value.args[
+            0] == "Validation failed: Invalid annotation type sent: object. Allowed values in project: [object-detection, signs, QA]"
