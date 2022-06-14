cd1bba72086e4b87cc6e34969941cfc7d20ce71c
Peter Newman <peter.newman@annotell.com>
2022-06-14 13:25:52 +0200

    First cut of LIDAR FoV support

--- annotell-input-api/annotell/input_api/model/calibration/lidar/lidar_calibration.py
+++ annotell-input-api/annotell/input_api/model/calibration/lidar/lidar_calibration.py
@@ -1,5 +1,14 @@
+from typing import Optional
+
+from annotell.input_api.model.base_serializer import BaseSerializer
 from annotell.input_api.model.calibration.common import BaseCalibration, CalibrationType
+
+
+class LidarFieldOfView(BaseSerializer):
+    start_angle: float
+    end_angle: float
 
 
 class LidarCalibration(BaseCalibration):
     calibration_type = CalibrationType.LIDAR
+    field_of_view: Optional[LidarFieldOfView]
--- annotell-input-api/examples/calibration/create_lidar_calibration.py
+++ annotell-input-api/examples/calibration/create_lidar_calibration.py
@@ -1,8 +1,9 @@
-from annotell.input_api.model.calibration.lidar.lidar_calibration import LidarCalibration
+from annotell.input_api.model.calibration.lidar.lidar_calibration import LidarCalibration, LidarFieldOfView
 from annotell.input_api.model.calibration.common import Position, RotationQuaternion
-
+from math import pi
 
 def unity_lidar_calibration():
     lidar_position = Position(x=0.0, y=0.0, z=0.0)
     lidar_rotation = RotationQuaternion(w=1.0, x=0.0, y=0.0, z=0.0)
-    return LidarCalibration(position=lidar_position, rotation_quaternion=lidar_rotation)
+    lidar_fov = LidarFieldOfView(start_angle=0, end_angle=2*pi)
+    return LidarCalibration(position=lidar_position, rotation_quaternion=lidar_rotation, field_of_view=lidar_fov)
