b10a9abdca72a3695c37dd7ce04a1256502ada7b
Peter Newman <peter.newman@annotell.com>
2022-06-15 16:42:42 +0200

    Tweak example values

--- annotell-input-api/examples/calibration/create_lidar_calibration.py
+++ annotell-input-api/examples/calibration/create_lidar_calibration.py
@@ -4,5 +4,5 @@ from annotell.input_api.model.calibration.common import Position, RotationQuater
 def unity_lidar_calibration():
     lidar_position = Position(x=0.0, y=0.0, z=0.0)
     lidar_rotation = RotationQuaternion(w=1.0, x=0.0, y=0.0, z=0.0)
-    lidar_fov = LidarFieldOfView(start_angle_deg=0, stop_angle_deg=360, depth=200)
+    lidar_fov = LidarFieldOfView(start_angle_deg=315, stop_angle_deg=45, depth=200)
     return LidarCalibration(position=lidar_position, rotation_quaternion=lidar_rotation, field_of_view=lidar_fov)
